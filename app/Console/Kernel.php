c4c1b7da857ab0fbae5c5f5a7cab831c29907cb9
b3d0NA <assasincreed2224@gmail.com>
2022-01-14 18:19:21 +0600

    Updated schedule task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Claim;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            Claim::where('created_at', '<=', now()->subDays(14)->toDateTimeString())
+                    ->whereNull('decision')
+                    ->update(["is_escalated" => 1]);
+        })->everyMinute();
     }
 
     /**
