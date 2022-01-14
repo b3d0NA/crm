7711ff4bb90298d28352303f9129c8841fa8fde4
b3d0NA <assasincreed2224@gmail.com>
2022-01-14 20:43:27 +0600

    After npm production

--- public/assets/plugins/ace-builds/mode-text.js
+++ public/assets/plugins/ace-builds/mode-text.js
@@ -1,8 +1,1 @@
-;                (function() {
-                    window.require(["ace/mode/text"], function(m) {
-                        if (typeof module == "object" && typeof exports == "object" && module) {
-                            module.exports = m;
-                        }
-                    });
-                })();
-            
+window.require(["ace/mode/text"],(function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)}));
